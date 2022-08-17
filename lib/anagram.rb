dcd52407a3e3dec793475068de6482903b666553
Patrick <ruomungai@gmail.com>
2022-08-17 15:15:37 +0300

    commit tree 233f99c0f4e45a785a9bc0cfde746e63f98ce8d3

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    arr.filter do |element|
+      element.chars.sort == word.chars.sort
+    end
+  end
+end
+
+gleoog = Anagram.new("gleoog")
+gleoog.match(%w[enlists google inlets banana])
